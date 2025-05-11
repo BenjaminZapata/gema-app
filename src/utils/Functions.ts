2a5ded48aa375cade98217527014052825726e85
Benjamin Zapata <benjaminzapata1998@gmail.com>
2025-05-11 03:32:04 -0300

    Funcion sin uso

--- src/utils/Functions.ts
+++ src/utils/Functions.ts
@@ -66,26 +66,3 @@ export const cleanObjectNullValues = (
     Object.entries(object).filter(([, value]) => value)
   );
 };
-
-export function stringifyBigIntsInObject(obj: any): any {
-  if (obj === null || typeof obj !== "object") {
-    return obj;
-  }
-  if (Array.isArray(obj)) {
-    return obj.map(stringifyBigIntsInObject);
-  }
-  const newObj: { [key: string]: any } = {};
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      const value = obj[key];
-      if (typeof value === "bigint") {
-        newObj[key] = value.toString();
-      } else if (typeof value === "object") {
-        newObj[key] = stringifyBigIntsInObject(value); // Recursividad para objetos/arrays anidados
-      } else {
-        newObj[key] = value;
-      }
-    }
-  }
-  return newObj;
-}
